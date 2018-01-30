5a946cb1d29d82c6c2bbd8d79754cba4d36fd62d
Kevin Andre Hansen <kevin.hansen@abax.no>
2018-01-30 17:57:41 +0100

    :D

---
+++ src/components/Pagecomponents/Messageboxes/MessageBox.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "message-box"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Pagecomponents/Topbanners/TopBanner.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "top-banner"
+    }
+</script>
+
+<style scoped>
+
+</style>
